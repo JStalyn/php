5b066be7ee1c644662dfe16583654a6d66addef3
Stalyn <estalinxk3gmail.com>
2022-04-23 16:13:17 -0500

    sintaxis básica, más las configuraciones para que funcione php es decir...  que se tiene que hacer para que funcione git  con xamp instalado

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+   echo "ingresa 3 números enteros separados por coma;";
+   fscanf(STDIN, "%d,%d,%d", $n1, $n2, $n3);
+   echo "el primer número es $n1 y el dos $n2 y $n3";
+ 
+        // $personas =[
+        //     "carlos" => 220,
+        //     "Mr. Michi" => 15,
+        //     "juan" => 65
+        // ];
+        // var_dump($personas);
+        // echo "\n"     
+?>
